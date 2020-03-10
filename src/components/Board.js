872f804067f960adb2b5916d89aae117f4aa0dc3
lmachens <leon.machens@googlemail.com>
2020-03-10 15:06:29 +0100

    Taking Turns

--- src/components/Board.js
+++ src/components/Board.js
@@ -3,12 +3,15 @@ import Square from "./Square";
 
 export default function Board() {
   const [squares, setSquares] = React.useState(Array(9).fill(null));
+  const [xIsNext, setXIsNext] = React.useState(true);
 
-  const status = "Next player: X";
+  const player = xIsNext ? "🛩" : "⛽️";
+  const status = `Next player: ${player}`;
 
   function handleClick(squareIndex) {
     const squaresCopy = squares.slice();
-    squaresCopy[squareIndex] = "X";
+    squaresCopy[squareIndex] = player;
+    setXIsNext(!xIsNext);
     setSquares(squaresCopy);
   }
 
